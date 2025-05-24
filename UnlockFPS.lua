687cad2b37484d0321ab5dbbd0d24cfbd034d324
Efield44444235364 <efi3lx@gmail.com>
2025-05-24 19:01:25 +0700

    Update UnlockFPS.lua

--- UnlockFPS.lua
+++ UnlockFPS.lua
@@ -1,21 +1,56 @@
-local fps = "120"
-local SetFps = loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Ff/refs/heads/main/Fpaload2(none).lua"))()
+local Players = game:GetService("Players")
+local StarterGui = game:GetService("StarterGui")
 
-local executor = identifyexecutor and identifyexecutor() or "Unknown Executor"
+local player = Players.LocalPlayer
 
-pcall(function()
-    game.StarterGui:SetCore("SendNotification", {
-        Title = executor .. " Executer Script Licence",
-        Text = "FPS Unlock",
-        Duration = 5
-    })
-end)
+-- executor check
+local executorName = "Unknown"
+if identifyexecutor then
+	local name = identifyexecutor()
+	if typeof(name) == "string" then
+		executorName = name
+	end
+end
 
-spawn(function()
-    while true do
-        pcall(function()
-            SetFps(fps)  
-        end)
-        wait(0.0000001)
-    end
-end)
+--krnl is kick
+if executorName == "Krnl" then
+wait(2.5)
+StarterGui:SetCore("SendNotification", {
+	Title = player.name .. "r u use Krnl???",
+	Text = "bitch",
+	Duration = 5
+})
+     wait(2)
+	game.Players.LocalPlayer:Kick("r u use Krnl?? go to sleep bitch krnl cant unlock fps i try it \n trust me u idiot clown")
+end
+
+-- codex kick
+if executorName == "Codex" then
+wait(2.5)
+StarterGui:SetCore("SendNotification", {
+	Title = player.name .. "r u use codex????",
+	Text = "fuck u",
+	Duration = 5
+})
+     wait(2)
+	game.Players.LocalPlayer:Kick("codex cant unlock fps bitch hahahaha clown")
+end
+
+local bindable = Instance.new("BindableFunction")
+function bindable.OnInvoke(response)
+	if response == "Yes" then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Ff/refs/heads/main/Fpsload1(none).lua"))()
+	elseif response == "No" then
+		print('อิห่านี่ ' .. player.Name .. ' เสือกกด "No" เฉย')
+	end
+end
+
+-- notification 
+StarterGui:SetCore("SendNotification", {
+	Title = "Boost FPS??",
+	Text = player.Name .. " | " .. executorName .. " Edition right licence",  -- เพิ่มชื่อผู้เล่นและ executor
+	Duration = 200000000,
+	Callback = bindable,
+	Button1 = "Yes",
+	Button2 = "No"
+})
