a56269a1efb54725e31d12b6813bb9dc620d6538
Efield44444235364 <efi3lx@gmail.com>
2025-06-27 13:28:06 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+--Notifunction
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Anti-ban/refs/heads/main/Noticing.lua"))()
+
+--Optimize Roblox
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Roblox/refs/heads/main/opt.lua"))()
